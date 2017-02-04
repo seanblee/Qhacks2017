b77f7fa129308ab6c0835eb30868d74ec54dbce4
15ay3 <andus401@gmail.com>
2017-02-04 17:57:39 -0500

    Loads create post page when button is clicked

---
+++ ease/app/src/main/java/com/seanlee/ease/CreatePostActivity.java
@@ -0,0 +1,24 @@
+package com.seanlee.ease;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by Andus on 2/4/2017.
+ */
+
+public class CreatePostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.create_post);
+
+    }
+
+}
