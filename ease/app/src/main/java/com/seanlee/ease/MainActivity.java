3e7b4bc75ecd05c8a0c2b4eda9a4af1fc402e461
seanseanleelee <seanseanleelee@github.com>
2017-02-04 01:05:46 -0500

    added firebase support

--- ease/app/src/main/java/com/seanlee/ease/MainActivity.java
+++ ease/app/src/main/java/com/seanlee/ease/MainActivity.java
@@ -2,12 +2,17 @@ package com.seanlee.ease;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.load_screen);
+    }
+
+    public void SignUp(View view){
+        setContentView(R.layout.sign_up);
     }
 }
