a23ba4402da83eb529074ae1c89f8d5db3178e1a
Darian Lio <darianlio97@gmail.com>
2017-02-05 20:14:45 -0500

    tutorial for profile

--- ease/app/src/main/java/com/seanlee/ease/UserProfile.java
+++ ease/app/src/main/java/com/seanlee/ease/UserProfile.java
@@ -1,6 +1,9 @@
 package com.seanlee.ease;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class UserProfile extends AppCompatActivity {
@@ -8,5 +11,8 @@ public class UserProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        Intent i = new Intent(UserProfile.this, IntroActivity.class);
+        startActivity(i);
     }
 }
